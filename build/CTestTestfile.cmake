ebd631294ebfd6b9162fb904fcbb370e43ad94b7
taemin-steve <okchrisok@naver.com>
2023-03-09 10:13:55 +0900

    .

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/ComputerVision/Computer-Vision
+# Build directory: C:/ComputerVision/Computer-Vision/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
